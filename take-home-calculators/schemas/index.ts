aaedcf6ce3a7ac0a5b8702a199a92c6b59d2f194
Forbes India <ppk9059@gmail.com>
2026-06-20 22:22:20 +0530

    Create index.ts

---
+++ take-home-calculators/schemas/index.ts
@@ -0,0 +1,5 @@
+import { siteSettings }      from "./siteSettings";
+import { pageMeta }          from "./pageMeta";
+import { homepageSections }  from "./homepageSections";
+
+export const schemaTypes = [siteSettings, pageMeta, homepageSections];
