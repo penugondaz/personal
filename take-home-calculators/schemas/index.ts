3725ea3bfdff1352420868575a51522e7255c32d
Forbes India <ppk9059@gmail.com>
2026-06-20 22:46:23 +0530

    Delete take-home-calculators/schemas/index.ts

--- take-home-calculators/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import { siteSettings }      from "./siteSettings";
-import { pageMeta }          from "./pageMeta";
-import { homepageSections }  from "./homepageSections";
-
-export const schemaTypes = [siteSettings, pageMeta, homepageSections];
