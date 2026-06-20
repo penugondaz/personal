9b401ae8df7d10b79a0863c88a79f19431fa7b48
Forbes India <ppk9059@gmail.com>
2026-06-20 18:05:37 +0000

    Add Sanity Studio deployment config

---
+++ take-home-calculators/sanity.cli.ts
@@ -0,0 +1,1 @@
+import { defineCliConfig } from "sanity/cli"; export default defineCliConfig({ api: { projectId: "c5v8ecsd", dataset: "production" }, deployment: { appId: "kj7965qgyhbu6cjeeky1m5h4" } });
