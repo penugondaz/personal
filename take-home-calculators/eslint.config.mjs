7abe92bb344d20991e9b2ef72b678233651adebc
Forbes India <ppk9059@gmail.com>
2026-06-26 11:16:55 +0530

    Update eslint.config.mjs

--- take-home-calculators/eslint.config.mjs
+++ take-home-calculators/eslint.config.mjs
@@ -1,12 +1,16 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
-
-const eslintConfig = [
-  ...nextCoreWebVitals,
-  ...nextTypescript,
-  {
-    ignores: ["out/**", "node_modules/**", ".next/**"],
-  },
-];
-
-export default eslintConfig;
+> next build
+⚠ No build cache found. Please configure build caching for faster rebuilds. Read more: https://nextjs.org/docs/messages/no-cache
+   ▲ Next.js 15.3.9
+   Creating an optimized production build ...
+ ✓ Compiled successfully in 8.0s
+   Linting and checking validity of types ...
+ ⨯ ESLint: Cannot find module '/home/runner/work/personal/personal/take-home-calculators/node_modules/eslint-config-next/core-web-vitals' imported from /home/runner/work/personal/personal/take-home-calculators/eslint.config.mjs Did you mean to import "eslint-config-next/core-web-vitals.js"?
+   We detected TypeScript in your project and reconfigured your tsconfig.json file for you.
+   The following mandatory changes were made to your tsconfig.json:
+   	- jsx was set to preserve (next.js implements its own optimized jsx transform)
+   Collecting page data ...
+   Generating static pages (0/63) ...
+   Generating static pages (15/63) 
+   Generating static pages (31/63) 
+   Generating static pages (47/63) 
+Failed to build /calculator/epf-vs-ppf/page: /calculator/epf-vs-ppf (attempt 1 of 3) because it took more than 60 seconds. Retrying again shortly.
