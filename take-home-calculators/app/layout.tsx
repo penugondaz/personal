7409e24ef9d42e7d0f0dd3dc0fc471bb86a34861
Forbes India <ppk9059@gmail.com>
2026-06-26 10:59:57 +0530

    Update layout.tsx

--- take-home-calculators/app/layout.tsx
+++ take-home-calculators/app/layout.tsx
@@ -38,6 +38,9 @@ export default function RootLayout({
       lang="en"
       className={`h-full antialiased ${fraunces.variable} ${inter.variable}`}
     >
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,500;0,600;0,700;1,500;1,600;1,700&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       <body className="min-h-full flex flex-col font-sans">
         <SiteShell>{children}</SiteShell>
 
