6ad7c5b201ec00c2a752c89456f5956be81e2f97
Forbes India <ppk9059@gmail.com>
2026-06-26 11:03:12 +0530

    Update salary-data.ts

--- take-home-calculators/lib/salary-data.ts
+++ take-home-calculators/lib/salary-data.ts
@@ -1,4 +1,4 @@
-export const SALARY_LPA_VALUES = [3, 4, 5, 6, 7, 7.5, 8, 9, 10, 11, 12, 12.5, 13, 14, 15, 15.5, 16, 17, 18, 20, 22.5, 25, 30, 40, 50];
+export const SALARY_LPA_VALUES = [5, 10, 15];
 
 export function salarySlug(lpa: number): string {
   return `${String(lpa).replace(".", "-")}-lpa-in-hand`;
